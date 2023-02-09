719584f973b4bb795baf85e9ae24856ad63767ca
zephiroth <braulio_3ri@hotmail.com>
2023-02-08 20:56:39 -0600

    initial

---
+++ Dockerfile
@@ -0,0 +1,14 @@
+FROM alpine:latest
+
+RUN apk update && \
+    apk add --no-cache nodejs npm
+
+WORKDIR /app
+COPY package.json ./
+RUN npm install
+COPY . .
+
+EXPOSE 5500
+
+CMD ["node", "index.js"]
+
---
+++ index.js
@@ -0,0 +1,31 @@
+const WebSocket = require('ws');
+const spawn = require('child_process').spawn;
+
+const wss = new WebSocket.Server({ port: 5500 });
+
+wss.on('connection', (ws) => {
+  console.log('Client connected');
+  const bash = spawn('/bin/bash', ['-i']);
+  bash.stdout.on('data', (data) => {
+    ws.send(data.toString(), (error) => {
+      if (error) {
+        console.error(`Error sending data: ${error}`);
+      }
+    });
+  });
+  bash.stderr.on('data', (data) => {
+    ws.send(data.toString(), (error) => {
+      if (error) {
+        console.error(`Error sending data: ${error}`);
+      }
+    });
+  });
+  ws.on('message', (message) => {
+    bash.stdin.write(`${message}\n`);
+  });
+  ws.on('close', () => {
+    console.log('Client disconnected');
+    bash.kill();
+  });
+});
+
